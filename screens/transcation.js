9fa78a8bc2147c809ad3040e62ae30a34b5b23d9
romilshah803 <74334375+romilshah803@users.noreply.github.com>
2021-08-03 21:45:44 -0400

    Add files via upload

--- screens/transcation.js
+++ screens/transcation.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Text,View,TouchableOpacity,StyleSheet}from 'react-native';
+import {Text,View,TouchableOpacity,StyleSheet,Image,TextInput}from 'react-native';
 import * as Permissions from 'expo-permissions'
 import{BarCodeScanner}from 'expo-barcode-scanner'
 export default class Transactions extends React.Component{
@@ -8,28 +8,37 @@ export default class Transactions extends React.Component{
     this.state={
       hasPermission:null,
       scanned:false,
-      scanneddata:'',
+      scannedstudentid:'',
+      scannedbookid:'',
       buttonstate:'normal'
     }
   }
-  getCameraPermissions=async()=>{
+  getCameraPermissions=async(id)=>{
     const {status}=await Permissions.askAsync(Permissions.CAMERA)
     this.setState({
       hasPermission:status==="granted",
-      buttonstate:'clicked',
+      buttonstate:id,
       scanned:false
     })
   }
   handlebarcodescan=async({type,data})=>{
-    this.setState({
-      scanneddata:data,buttonstate:'normal',scanned:true
-    })
+    const {buttonstate}=this.state
+    if(buttonstate=="bookid"){
+      this.setState({
+        scannedbookid:data,buttonstate:'normal',scanned:true
+      })
+    }else{
+      this.setState({
+        scannedstudentid:data,buttonstate:'normal',scanned:true
+      })
+    }
+    
   }
   render(){
     const hasPermission=this.state.hasPermission;
     const scanned=this.state.scanned;
     const buttonstate=this.state.buttonstate;
-    if(buttonstate==="clicked" && hasPermission){
+    if(buttonstate!=="normal" && hasPermission){
       return(
         <BarCodeScanner
         onBarCodeScanned={scanned?undefined:this.handlebarcodescan}
@@ -39,10 +48,45 @@ export default class Transactions extends React.Component{
     }else{
       return(
         <View style={styles.container}>
-        <Text style={styles.dt} >{hasPermission==true?this.state.scanneddata:"request camera permissions"} </Text>
-        <TouchableOpacity style={styles.scannedButton}
-        onPress={this.getCameraPermissions}>
-          <Text style={styles.buttonText}>scan qr code</Text>
+          <View>
+            <Image source={require("../assets/booklogo.jpg")}
+            style={{width:200,height:200}}
+            />
+            
+          </View>
+          <View style={styles.inputview}>
+            <TextInput
+            style={styles.inputbox}
+            placeholder="book id"
+            value={this.state.scannedbookid}
+            onChangeText={(text)=>{this.setState({scannedbookid:text})}}
+            >
+
+            </TextInput>
+            <TouchableOpacity onPress={()=>{this.getCameraPermissions("bookid")}} style={styles.scannedButton}
+            >
+             <Text>scan</Text> 
+            </TouchableOpacity>
+            </View>
+          <View style={styles.inputview}>
+            <TextInput
+                        style={styles.inputbox}
+
+            placeholder="student id"
+            value={this.state.scannedstudentid}
+            onChangeText={(text)=>{this.setState({scannedstudentid:text})}}
+            >
+
+            </TextInput>
+            <TouchableOpacity style={styles.scannedButton}onPress={()=>{this.getCameraPermissions("studentid")}}
+            >
+             <Text>scan</Text> 
+            </TouchableOpacity>
+          </View>
+        <Text style={styles.dt} >{hasPermission==true?this.state.scannedbookid:"request camera permissions"} </Text>
+        <TouchableOpacity style={styles.submitbutton}
+        onPress={}>
+          <Text style={styles.buttonText}>submit</Text>
         </TouchableOpacity>
         </View>
 
@@ -65,8 +109,20 @@ const styles=StyleSheet.create({
   },
   scannedButton:{
     backgroundColor:'#2196F3',
-    padding:10,
-    margin:10
+    width:50,
+    borderWidth:1.5,
+    borderLeftWidth:0
 
+  },
+  inputview:{
+    flexDirection:'row',
+    margin:20
+  },
+  inputbox:{
+    width:200,height:40,borderWidth:1.5,borderRightWidth:0,fontSize:20
+  },
+  submitbutton:{
+    BackgroundColor:'#66886a',
+    width:80,
   }
 })
